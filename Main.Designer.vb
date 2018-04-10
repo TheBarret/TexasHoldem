d3ddcc53be5539e627fd6b68f70ec4f7530fd1fc
Barret <format.e.drive@gmail.com>
2018-04-11 00:07:45 +0200

    Add files via upload

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -26,13 +26,23 @@ Partial Class Main
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Display5 = New Poker.Display()
+        Me.Display4 = New Poker.Display()
+        Me.Display3 = New Poker.Display()
+        Me.Display2 = New Poker.Display()
+        Me.Display1 = New Poker.Display()
+        Me.DisplayTable = New Poker.Display()
+        Me.DisplayPlayer = New Poker.Display()
         Me.SuspendLayout()
         '
         'btnDeal
         '
-        Me.btnDeal.Location = New System.Drawing.Point(12, 131)
+        Me.btnDeal.Location = New System.Drawing.Point(216, 233)
         Me.btnDeal.Name = "btnDeal"
-        Me.btnDeal.Size = New System.Drawing.Size(286, 42)
+        Me.btnDeal.Size = New System.Drawing.Size(190, 38)
         Me.btnDeal.TabIndex = 0
         Me.btnDeal.Text = "Deal"
         Me.btnDeal.UseVisualStyleBackColor = True
@@ -40,41 +50,131 @@ Partial Class Main
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(45, 40)
+        Me.Label1.Location = New System.Drawing.Point(213, 205)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(49, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Player:"
+        Me.Label1.Size = New System.Drawing.Size(16, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "..."
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(51, 67)
+        Me.Label2.Location = New System.Drawing.Point(17, 74)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(43, 13)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Table:"
+        Me.Label2.Size = New System.Drawing.Size(16, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "..."
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(57, 95)
+        Me.Label3.Location = New System.Drawing.Point(213, 74)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(37, 13)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Hand:"
+        Me.Label3.Size = New System.Drawing.Size(16, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "..."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(409, 74)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(16, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "..."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 205)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(16, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "..."
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(409, 205)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(16, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "..."
+        '
+        'Display5
+        '
+        Me.Display5.BackColor = System.Drawing.Color.White
+        Me.Display5.Location = New System.Drawing.Point(412, 164)
+        Me.Display5.Name = "Display5"
+        Me.Display5.Size = New System.Drawing.Size(190, 38)
+        Me.Display5.TabIndex = 15
+        '
+        'Display4
+        '
+        Me.Display4.BackColor = System.Drawing.Color.White
+        Me.Display4.Location = New System.Drawing.Point(20, 164)
+        Me.Display4.Name = "Display4"
+        Me.Display4.Size = New System.Drawing.Size(190, 38)
+        Me.Display4.TabIndex = 13
+        '
+        'Display3
+        '
+        Me.Display3.BackColor = System.Drawing.Color.White
+        Me.Display3.Location = New System.Drawing.Point(412, 33)
+        Me.Display3.Name = "Display3"
+        Me.Display3.Size = New System.Drawing.Size(190, 38)
+        Me.Display3.TabIndex = 9
+        '
+        'Display2
+        '
+        Me.Display2.BackColor = System.Drawing.Color.White
+        Me.Display2.Location = New System.Drawing.Point(216, 33)
+        Me.Display2.Name = "Display2"
+        Me.Display2.Size = New System.Drawing.Size(190, 38)
+        Me.Display2.TabIndex = 8
+        '
+        'Display1
+        '
+        Me.Display1.BackColor = System.Drawing.Color.White
+        Me.Display1.Location = New System.Drawing.Point(20, 33)
+        Me.Display1.Name = "Display1"
+        Me.Display1.Size = New System.Drawing.Size(190, 38)
+        Me.Display1.TabIndex = 7
+        '
+        'DisplayTable
+        '
+        Me.DisplayTable.BackColor = System.Drawing.Color.White
+        Me.DisplayTable.Location = New System.Drawing.Point(216, 103)
+        Me.DisplayTable.Name = "DisplayTable"
+        Me.DisplayTable.Size = New System.Drawing.Size(190, 38)
+        Me.DisplayTable.TabIndex = 5
+        '
+        'DisplayPlayer
+        '
+        Me.DisplayPlayer.BackColor = System.Drawing.Color.White
+        Me.DisplayPlayer.Location = New System.Drawing.Point(216, 164)
+        Me.DisplayPlayer.Name = "DisplayPlayer"
+        Me.DisplayPlayer.Size = New System.Drawing.Size(190, 38)
+        Me.DisplayPlayer.TabIndex = 4
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(310, 188)
+        Me.ClientSize = New System.Drawing.Size(617, 284)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Display5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Display4)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Display3)
+        Me.Controls.Add(Me.Display2)
+        Me.Controls.Add(Me.Display1)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DisplayTable)
+        Me.Controls.Add(Me.DisplayPlayer)
         Me.Controls.Add(Me.btnDeal)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
@@ -87,8 +187,18 @@ Partial Class Main
 
     End Sub
     Friend WithEvents btnDeal As System.Windows.Forms.Button
+    Friend WithEvents DisplayPlayer As Poker.Display
+    Friend WithEvents DisplayTable As Poker.Display
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Display1 As Poker.Display
+    Friend WithEvents Display2 As Poker.Display
+    Friend WithEvents Display3 As Poker.Display
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Display4 As Poker.Display
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Display5 As Poker.Display
 
 End Class
--- Main.vb
+++ Main.vb
@@ -2,8 +2,9 @@
 
 Public Class Main
     Private Property Deck As Deck
-    Private Property Player As Hand
-    Private Property Table As List(Of Card)
+    Private Property Self As Hand
+    Private Property Table As Hand
+    Private Property Players As List(Of Hand)
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.btnDeal.PerformClick()
     End Sub
@@ -12,17 +13,41 @@ Public Class Main
         Me.Deck = New Deck(True)
 
         '// Create player
-        Me.Player = New Hand(Me.Deck.Deal(5))
+        Me.Self = New Hand(Me.Deck.Deal(5))
+
+        Me.Players = New List(Of Hand)
+        Me.Players.Add(New Hand(Me.Deck.Deal(5)))
+        Me.Players.Add(New Hand(Me.Deck.Deal(5)))
+        Me.Players.Add(New Hand(Me.Deck.Deal(5)))
+        Me.Players.Add(New Hand(Me.Deck.Deal(5)))
+        Me.Players.Add(New Hand(Me.Deck.Deal(5)))
 
         '// Create table with additional 5 cards
-        Me.Table = Me.Deck.Deal(5)
+        Me.Table = New Hand(Me.Deck.Deal(2))
 
-        '// Evaluate hand + table
-        Me.Player.Evaluate(Me.Table)
+        Me.DisplayTable.Assign(Me.Table)
 
-        Me.Label1.Text = String.Format("Player: {0}", String.Join(" ", Me.Player.Cards.Select(Function(x) x.ToString)))
-        Me.Label2.Text = String.Format("Table: {0}", String.Join(" ", Me.Table.Select(Function(x) x.ToString)))
-        Me.Label3.Text = String.Format("Hand: {0} [{1}]", Me.Player.ToString, Me.Player.Highcard)
+        '// Evaluate hands
+        Me.Players(0).EvaluateWith(Me.Table.Cards)
+        Me.Players(1).EvaluateWith(Me.Table.Cards)
+        Me.Players(2).EvaluateWith(Me.Table.Cards)
+        Me.Players(3).EvaluateWith(Me.Table.Cards)
+        Me.Players(4).EvaluateWith(Me.Table.Cards)
 
+        Me.Display1.Assign(Me.Players(0))
+        Me.Display2.Assign(Me.Players(1))
+        Me.Display3.Assign(Me.Players(2))
+        Me.Display4.Assign(Me.Players(3))
+        Me.Display5.Assign(Me.Players(4))
+
+        Me.Label2.Text = Me.Players(0).ToString
+        Me.Label3.Text = Me.Players(1).ToString
+        Me.Label4.Text = Me.Players(2).ToString
+        Me.Label5.Text = Me.Players(3).ToString
+        Me.Label6.Text = Me.Players(4).ToString
+
+        Me.Self.EvaluateWith(Me.Table.Cards)
+        Me.DisplayPlayer.Assign(Me.Self)
+        Me.Label1.Text = Me.Self.ToString
     End Sub
 End Class
